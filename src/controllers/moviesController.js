6130f55a16dafefadcba6a2b5ddbdaf2e0539601
santiperone <sdperone97@gmail.com>
2020-11-05 23:35:40 -0300

    creadas vistas new y recommended

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const db = require('../data/models')
 const moment = require('moment');
+const { Op } = require("sequelize");
 
 module.exports = {
     all: async function(req, res) {
@@ -10,16 +11,40 @@ module.exports = {
         } catch (error) {
             res.send(error.message)
         }
-      },
+    },
     detail: async function(req, res) {
         try {
             let movie = await db.Movie.findByPk(req.params.id);
             movie = movie.dataValues;
             movie.release_date = moment(movie.release_date).format('DD/MM/YYYY');
-            console.log(movie.release_date);
+            // console.log(movie.release_date);
             res.render('movieDetail', { movie });
         } catch (error) {
             res.send(error.message)
         }
-      },
+    },
+    recom: async function(req, res) {
+        try {
+            const movies = await db.Movie.findAll({where: {rating: {[Op.gt]: 8}}});
+            res.render('movies', { movies })
+        } catch (error) {
+            res.send(error.message)
+        }
+    },
+    new: async function(req, res) {
+        try {
+            const movies = await db.Movie.findAll({order: [['release_date', 'DESC']], limit: 5});
+            res.render('movies', { movies })
+        } catch (error) {
+            res.send(error.message)
+        }
+    },
+    search: async function(req, res) {
+        try {
+            const movies = await db.Movie.findAll({where: {title: {[Op.like]: '%'+ +'%'}}});
+            res.render('movies', { movies })
+        } catch (error) {
+            res.send(error.message)
+        }
+    },
 }
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -5,5 +5,7 @@ const controller = require('../controllers/moviesController');
 /* GET users listing. */
 router.get('/', controller.all);
 router.get('/detail/:id', controller.detail);
+router.get('/recommended', controller.recom);
+router.get('/new', controller.new);
 
 module.exports = router;
